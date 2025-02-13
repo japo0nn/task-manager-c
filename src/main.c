9a51edc411eb2a6d9310c2f69f8fd1bdf2f86ff8
japo0nn <igris@DESKTOP-50GFNGO.>
2025-02-14 00:54:54 +0500

    added: display updation & handle_sigint func

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
+#include <signal.h>
+
+#include "stdlib.h"
 #include "task_manager.h"
 #include "unistd.h"
 
 void clear_console();
+void handle_sigint(int sig);
 
 int main(void) {
+	signal(SIGINT, handle_sigint);
+	printf("\033[2J");
 	int i = 0;
 	while (i < 10) {
-		list_proccesses();
 		clear_console();
 		printf("i = %d\n", i);
 		sleep(3);
@@ -15,4 +20,11 @@ int main(void) {
 	return 0;
 }
 
-void clear_console() { printf("\033[H\033[J"); }
+void clear_console() {
+	printf("\033[H\n");
+	int counter = 100;
+	list_proccesses();
+	fflush(stdout);
+}
+
+void handle_sigint(int sig) { exit(0); }  // TODO: SAFE exit!!!
